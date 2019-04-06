21486074c37e80bec689da63cf99e4d1e103e1cc
Winston Hartnett <winstonhartnett@gmail.com>
2019-04-06 16:38:18 -0500

    Logistic driving now observes max speed

--- robotmap.py
+++ robotmap.py
@@ -33,7 +33,7 @@ spd_chassis_drive = 0.55
 spd_chassis_rotate = 0.4
 spd_chassis_vision_seeking = 0.3
 
-accel_chassis_max = 5.5
+accel_chassis_max = 5.0
 decel_chassis_max = 4.0
 
 spd_intake = 0.5
