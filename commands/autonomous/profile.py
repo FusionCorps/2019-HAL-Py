4c323d09d7b9ad36758c2c0b75df456dc7b3a462
Winston Hartnett <winstonhartnett@gmail.com>
2019-04-07 12:31:21 -0500

    Made profile.py more pythonic

--- commands/autonomous/profile.py
+++ commands/autonomous/profile.py
@@ -16,60 +16,34 @@ class ProfileGenerator(object):
 
     def generate(self, *args, **kwargs):
         self.logger.warning(f"ProfileGenerator called with args: {args}. Called with kwargs: {kwargs}.")
-        v, a, j, name = None, None, None, None
-
         points = [pf.Waypoint(loc[0], loc[1], loc[2]) for loc in args]
-
+        conditions = {'name': f"{str(args).strip(' ')}", 'v': robotmap.chassis_max_vel,
+                      'a': robotmap.chassis_max_acceleration, 'j': robotmap.chassis_max_jerk}
         for key, value in kwargs.items():
-            if key == 'name':
-                if value is not None:
-                    name = value
-                else:
-                    name = f"{str(args).strip(' ')}"
-            elif key == 'v':
-                if value is not None:
-                    v = float(value)
-                else:
-                    v = robotmap.chassis_max_vel
-            elif key == 'a':
-                if value is not None:
-                    a = float(value)
-                else:
-                    a = robotmap.chassis_max_acceleration
-            elif key == 'j':
-                if j is not None:
-                    j = float(value)
-                else:
-                    j = robotmap.chassis_max_jerk
-            else:
-                continue
-
+            conditions.update(key=value)
         self.logger.warning(f"Requested points {str(points)}.")
 
         try:
-            info, trajectory = pf.generate(points, pf.FIT_HERMITE_CUBIC, pf.SAMPLES_HIGH, 0.05, v, a, j, )
-            self.logger.warning("Trajectory generated.")
-            pf.serialize_csv(f"AutoProfile_{name}", trajectory)
-            self.logger.warning("Trajectory saved.")
+            info, trajectory = pf.generate(points, pf.FIT_HERMITE_CUBIC, pf.SAMPLES_HIGH, 0.05, conditions.get('v'),
+                                           conditions.get('a'), conditions.get('j'), )
         except ValueError as e:
             self.logger.error(f"Trajectory generation failed! {e}")
+        else:
+            self.logger.warning("Trajectory generated.")
+            pf.serialize_csv(f"AutoProfile_{conditions.get('name')}", trajectory)
+            self.logger.warning("Trajectory saved.")
 
 
 class ProfileFollower(Command):
     def __init__(self, *args, **kwargs):
         self.args, self.kwargs = args, kwargs
         file_loc, file_name = None, None
-        for value in args:
-            pass
+
         for key, value in kwargs:
-            if key == 'file_loc':
-                file_loc = value
-            elif key == 'file_name':
-                file_name = value
-            elif key == 'generate':
-                self.generate = value
-            else:
-                continue
+            file_loc = value if key == 'file_loc' else None
+            file_name = value if key == 'file_name' else None
+            self.generate = value if key == 'generate' else False
+
         self.file_name = f"{file_loc}AutoProfile_{file_name}"
         super().__init__(f"{self.file_name}:^20")
         self.requires(subsystems.chassis)
@@ -135,7 +109,7 @@ class ProfileFollower(Command):
             subsystems.chassis.set_left((output_l - turn_output))
             subsystems.chassis.set_right((output_r - turn_output))
 
-            if self.timer.hasPeriodPassed(0.4):
+            if self.timer.hasPeriodPassed(1):
                 self.logger.info(f"Left is {round(output_l, 2): ^4}. Right is {round(output_r, 2): ^4}."
                                  f" Turn is {round(turn_output, 2): ^4}")
         else:
